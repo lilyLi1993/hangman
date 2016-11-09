19f71f0cb286b042a092917b2cbd8e83aef258c2
lilyLi1993 <lli2@smu.edu>
2016-11-09 12:05:33 -0600

    Update game.ex

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -1,52 +1,128 @@
 defmodule Hangman.Game do
 
-  @moduledoc """
 
-  This is the backend for a Hangman game. It manages the game state.
-  Clients make moves, and this code validates them and reports back
-  the updated state.
-
-  We have a well-defined API that is used by client code
-
-  * `game = Hangman.Game.new_game`
-
-    Set up the state for a new game, and return that state. The client
-    applications will pass this state back to your code in all the
-    subsequent API calls.
-
-    The state that's returned will at a minimum contain the word to be
-    guessed.
-
-    As an aid to testing, there's a second form of this function:
-
-    `game = Hangman.Game.new_game(word)`
-
-    This forces `word` to be this game's hidden word.
+  @spec new_game :: state
+  def new_game do
+    random_word = Hangman.Dictionary.random_word
+  %{
+    word: random_word,
+    turns_left: 10,
+    guessed: MapSet.new,
+    letter_left: dictionary_letters(random_word) 
+   }
+  end
 
 
-  * `len = Hangman.Game.word_length(game)`
 
-    Return the length of the current word.
+  def new_game(word) do
+  %{
+    word: word,
+    turns_left: 10,
+    guessed: MapSet.new,
+    letter_left: dictionary_letters(word) 
+   }
+  end
 
-  * `list = letters_used_so_far(game)`
 
-    The letters that have been guessed so far, returned as a list of
-    single character strings. (This includes both correct and
-    incorrect guessed.
+  @doc """
+  `{game, status, guess} = make_move(game, guess)`
+  Accept a guess. Return a three element tuple containing the updated
+  game state, an atom giving the status at the end of the move, and
+  the letter that was guessed.
+  The status can be:
+  * `:won` — the guess correct and completed the game. The client won
+  * `:lost` — the guess was incorrect and the client has run out of
+     turns. The game has been lost.
+  * `:good_guess` — the guess occurs one or more turns in the word
+  * `:bad_guess` — the word does not contain the guess. The number
+     of turns left has been reduced by 1
+  """
 
-  * `count = turns_left(game)`
+  @spec make_move(state, ch) :: { state, atom, optional_ch }
+  def make_move(state, guess) do
+    {match_guessed, state} = state |> pop_in([:letter_left, guess])
+    if !match_guessed do
+       state = %{state | turns_left: state.turns_left - 1}
+    end
+    state = %{state | guessed: MapSet.put(state.guessed, guess)}
+    cond do 
+        state.turns_left == 0
+      -> {state, :lost, nil}
+      !match_guessed
+      -> {state, :bad_guess, guess}
+      state.letter_left |> Map.keys |> length == 0
+      -> {state, :won, nil}
+      match_guessed
+      -> {state, :good_guess, guess}
 
-    Returns the number of turns remaining before the game is over.
-    For our purposes, a game starts with a generous 10 turns. Each
-    _incorrect_ guess decrements this.
+    # I tried the following codes at the first time, but it didn't work, maybe the value could not be transitted to the :lost and so on
+    #state.turns_left == 0
+    #-> :lost
+    #!match_guessed
+    #-> :bad_guess
+    #state.letter_left |> Map.keys |> length == 0
+    #-> :won
+    #match_guessed
+    #-> :good_guess
 
-  * `word = word_as_string(game, reveal \\ false)`
+    end
+  end
 
-     Returns the word to be guessed. If the optional second argument is
-     false, then any unguessed letters will be returned as underscores.
-     If it is true, then the word will be returned complete, showing
-     all letters. Letters and underscores are separated by spaces.
 
+  @doc """
+  `len = Hangman.Game.word_length(game)`
+  Return the length of the current word.
+  """
+  @spec word_length(state) :: integer
+  def word_length(%{ word: word }) do
+    String.length(word)
+  end
+
+  @doc """
+  `list = letters_used_so_far(game)`
+  The letters that have been guessed so far, returned as a list of
+  single character strings. (This includes both correct and
+  incorrect guessed.
+  """
+
+  @spec letters_used_so_far(state) :: [ binary ]
+  def letters_used_so_far(state) do
+    MapSet.to_list(state.guessed)
+  end
+
+  @doc """
+  `count = turns_left(game)`
+  #here I wanan changed the name, but seems like some functions are unique in the test.ex
+  Returns the number of turns remaining before the game is over.
+  For our purposes, a game starts with a generous 10 turns. Each
+  _incorrect_ guess decrements this.
+  """
+
+  @spec turns_left(state) :: integer
+  def turns_left(state) do
+    state.turns_left
+  end
+
+
+  @spec word_as_string(state, boolean) :: binary
+  def word_as_string(state, reveal \\ false) do
+    guessed = [" "| letters_used_so_far(state)] |> Enum.join
+    word = if !reveal do
+      String.replace(state.word, ~r/[^#{guessed}]/, "_")
+    else
+      state.word
+    end
+    word |> String.graphemes |> Enum.join(" ")
+  end
+
+  ###########################
+  # end of public interface #
+  ###########################
+
+  # Your private functions go here
+  defp dictionary_letters(word), do: word |> String.graphemes |> Map.new(&{&1, true})
+
+end
   *  `{game, status, guess} = make_move(game, guess)`
 
      Accept a guess. Return a three element tuple containing the updated
